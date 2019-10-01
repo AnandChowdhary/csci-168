85d26423543e44b0cedf10fcff85380873a2fdf8
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-01 10:06:45 -0700

    :sparkles: Add XCode project

---
+++ HW1/HW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW1
+//
+//  Created by Anand Chowdhary on 10/1/19.
+//  Copyright © 2019 Anand Chowdhary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
