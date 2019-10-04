18d6dc46635796699c078378a2a2c621f177b00d
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-04 13:39:54 -0700

    :sparkles: Add Homework 2

---
+++ HW2/HW1/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  HW1
+//
+//  Created by Anand Chowdhary on 10/1/19.
+//  Copyright © 2019 Anand Chowdhary. All rights reserved.
+//
+
+#define GL_SILENCE_DEPRECATION
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+
+void redraw() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
+    
+    //
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char * argv[]) {
+    srand(time(NULL));
+    std::cout << "Homework 2\n" << "Output" << "\n";
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("Homework 2");
+    glutPositionWindow(10,10);
+    glutReshapeWindow(700,700);
+    glClearColor(0.0,0.0,0.0,1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0,1.0,0.0,1.0,-10.0,10.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glutDisplayFunc(redraw);
+    glutMainLoop();
+    return 0;
+}
---
+++ HW2/README.md
@@ -0,0 +1,11 @@
+#  Homework 2
+
+Make a 2D scene with 4 connected triangles sharing one vertex in the center. [2 points]
+
+Each triangle should have a different color. [1 point]
+
+Each triangle edge should be drawn as a line. [1 point]
+
+Each vertex should be drawn as a point. [1 point]
+
+When the user clicks on or very near a vertex, the vertex moves with the mouse while the mouse button is down. [5 points]
