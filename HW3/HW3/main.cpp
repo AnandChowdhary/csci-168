99871592cba4d2efd249f0c04a35d69b50805d3b
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-03 18:16:40 -0800

    :sparkles: Complete step 1: Shapes

--- HW3/HW3/main.cpp
+++ HW3/HW3/main.cpp
@@ -18,193 +18,47 @@
 #include <stdlib.h>
 #include <math.h>
 
-float topLeftX = 0.1;
-float topLeftY = 0.9;
+int angle = 0;
 
-float topRightX = 0.9;
-float topRightY = 0.9;
+void drawRectangleOnPosition(float x, float y) {
+//    glTranslatef(0.5, 0.5, 0.5);
+//    glRotatef(angle, 0, 0, 1);
+//    glTranslatef(-0.5, -0.5, -0.5);
+    glColor3f(1, 1, 0);
+    glBegin(GL_POLYGON);
+    glVertex3f(x - 0.1, y + 0.05, 0.5);
+    glVertex3f(x + 0.1, y + 0.05, 0.5);
+    glVertex3f(x + 0.1, y - 0.05, 0.5);
+    glVertex3f(x - 0.1, y - 0.05, 0.5);
+    glEnd();
+}
 
-float centerX = 0.5;
-float centerY = 0.5;
-
-float bottomLeftX = 0.1;
-float bottomLeftY = 0.1;
-
-float bottomRightX = 0.9;
-float bottomRightY = 0.1;
+void drawSquare() {
+    glPushMatrix();
+    glColor3f(1, 0, 1);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.25, 0.25, 0.2);
+    glVertex3f(0.25, 0.75, 0.2);
+    glVertex3f(0.75, 0.75, 0.2);
+    glVertex3f(0.75, 0.25, 0.2);
+    glEnd();
+    glPopMatrix();
+}
 
 void redraw() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_DEPTH_TEST);
-    
-    // POLYGONS
-
-    // Top polygon
-    glColor3f(0, 0, 1);
-    glBegin(GL_POLYGON);
-    glVertex3f(topLeftX, topLeftY, 0.2);
-    glVertex3f(centerX, centerY, 0.2);
-    glVertex3f(topRightX, topRightY, 0.2);
-    glEnd();
-
-    // Right polygon
-    glColor3f(0, 1, 0);
-    glBegin(GL_POLYGON);
-    glVertex3f(topRightX, topRightY, 0.2);
-    glVertex3f(bottomRightX, bottomRightY, 0.2);
-    glVertex3f(centerX, centerY, 0.2);
-    glEnd();
-
-    // Bottom polygon
-    glColor3f(1, 0, 0);
-    glBegin(GL_POLYGON);
-    glVertex3f(bottomLeftX, bottomLeftY, 0.2);
-    glVertex3f(centerX, centerY, 0.2);
-    glVertex3f(bottomRightX, bottomRightY, 0.2);
-    glEnd();
-
-    // Left polygon
-    glColor3f(1, 1, 0);
-    glBegin(GL_POLYGON);
-    glVertex3f(topLeftX, topLeftY, 0.2);
-    glVertex3f(centerX, centerY, 0.2);
-    glVertex3f(bottomLeftX, bottomLeftY, 0.2);
-    glEnd();
-    
-    // LINES
-    glLineWidth(3);
-    
-    // Top
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(topLeftX, topLeftY, 0.3);
-    glVertex3f(topRightX, topRightY, 0.3);
-    glEnd();
-    
-    // Right
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(topRightX, topRightY, 0.3);
-    glVertex3f(bottomRightX, bottomRightY, 0.3);
-    glEnd();
-    
-    // Bottom
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(bottomLeftX, bottomLeftY, 0.3);
-    glVertex3f(bottomRightX, bottomRightY, 0.3);
-    glEnd();
-
-    // Left
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(topLeftX, topLeftY, 0.3);
-    glVertex3f(bottomLeftX, bottomLeftY, 0.3);
-    glEnd();
-
-    // Diagonal Top Left
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(topLeftX, topLeftY, 0.3);
-    glVertex3f(centerX, centerY, 0.3);
-    glEnd();
-
-    // Diagonal Top Right
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(topRightX, topRightY, 0.3);
-    glVertex3f(centerX, centerY, 0.3);
-    glEnd();
-
-    // Diagonal Bottom Left
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(bottomLeftX, bottomLeftY, 0.3);
-    glVertex3f(centerX, centerY, 0.3);
-    glEnd();
-
-    // Diagonal Bottom Right
-    glColor3f(1, 1, 1);
-    glBegin(GL_LINES);
-    glVertex3f(bottomRightX, bottomRightY, 0.3);
-    glVertex3f(centerX, centerY, 0.3);
-    glEnd();
-    
-    // POINTS
-    glPointSize(10);
-    glColor3f(0, 1, 1);
-    glBegin(GL_POINTS);
-    
-    glVertex3f(topLeftX, topLeftY, 0.5);
-    glVertex3f(topRightX, topRightY, 0.5);
-    glVertex3f(bottomLeftX, bottomLeftY, 0.5);
-    glVertex3f(bottomRightX, bottomRightY, 0.5);
-    glVertex3f(centerX, centerY, 0.5);
-
-    glEnd();
-
+    angle++;
+    drawSquare();
+    drawRectangleOnPosition(0.25, 0.75);
+    drawRectangleOnPosition(0.75, 0.75);
     glutSwapBuffers();
 }
 
-float diff(float x, float y, float X, float Y) {
-    return sqrt(pow((X - x), 2) + pow((Y - y), 2));
-}
-
-void mouseMove(int x, int y) {
-    float X = ((float) x / 700);
-    float Y = 1.0 - ((float) y / 700);
-    std::string selected = "top-left";
-    float minimumDistance = 1.0;
-    
-    // First, we select the point you want to move
-    if (diff(X, Y, topLeftX, topLeftY) < minimumDistance) {
-        selected = "top-left";
-        minimumDistance = diff(X, Y, topLeftX, topLeftY);
-    }
-    if (diff(X, Y, topRightX, topRightY) < minimumDistance) {
-        selected = "top-right";
-        minimumDistance = diff(X, Y, topRightX, topRightY);
-    }
-    if (diff(X, Y, bottomLeftX, bottomLeftY) < minimumDistance) {
-        selected = "bottom-left";
-        minimumDistance = diff(X, Y, bottomLeftX, bottomLeftY);
-    }
-    if (diff(X, Y, bottomRightX, bottomRightY) < minimumDistance) {
-        selected = "bottom-right";
-        minimumDistance = diff(X, Y, bottomRightX, bottomRightY);
-    }
-    if (diff(X, Y, centerX, centerY) < minimumDistance) {
-        selected = "center";
-        minimumDistance = diff(X, Y, centerX, centerY);
-    }
-    
-    std::cout << "MOUSE\n" << X << "  " << Y << " " << minimumDistance << " " << selected << "\n";
-
-    // Then, we move that point
-    if (selected == "top-left") {
-        topLeftX = X;
-        topLeftY = Y;
-    } else if (selected == "top-right") {
-        topRightX = X;
-        topRightY = Y;
-    } else if (selected == "bottom-left") {
-        bottomLeftX = X;
-        bottomLeftY = Y;
-    } else if (selected == "bottom-right") {
-        bottomRightX = X;
-        bottomRightY = Y;
-    } else {
-        centerX = X;
-        centerY = Y;
-    }
-
-    redraw();
-}
-
 int main(int argc, char * argv[]) {
-    std::cout << "Homework 2\n" << "Output" << "\n";
+    std::cout << "Homework 3\n" << "Output" << "\n";
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutCreateWindow("Homework 2");
@@ -217,7 +71,7 @@ int main(int argc, char * argv[]) {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glutDisplayFunc(redraw);
-    glutMotionFunc(mouseMove);
+    glutIdleFunc(redraw);
     glutMainLoop();
     return 0;
 }
