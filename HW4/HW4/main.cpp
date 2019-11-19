3d01a58e5dd8e29bfd237f1cebb4a4c079e28a68
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-18 17:36:22 -0800

    :construction: Add HW4

---
+++ HW4/HW4/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  HW4
+//
+//  Created by Anand Chowdhary on 10/1/19.
+//  Copyright © 2019 Anand Chowdhary. All rights reserved.
+//
+
+#define GL_SILENCE_DEPRECATION
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char * argv[]) {
+    std::cout << "Homework 3\n" << "Output" << "\n";
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("Homework 4");
+    glutPositionWindow(10,10);
+    glutReshapeWindow(700,700);
+    glClearColor(0.0,0.0,0.0,1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -10.0, 10.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glutMainLoop();
+    return 0;
+}
---
+++ HW4/README.md
@@ -0,0 +1,17 @@
+# Homework 4
+
+Write a Ray Tracer.
+
+[3 points] At least one sphere in the scene. Correct ray-sphere intersection causes pixels from which rays hit sphere to be colored.
+
+[3 points] At least one triangle in the scene. Correct ray-triangle intersection causes pixels from which rays hit triangle to be colored.
+
+[3 points] Triangle and Sphere correctly lighted by Ambient, Diffuse and Specular lighting according to Phong model.
+
+[1 point] At least one reflective object in the scene. Reflection implemented correctly.
+
+## Extra credit
+
+[1 point] Implement Shadow.
+
+[1 point] Implement Soft shadow.
